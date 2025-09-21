42d0da32a64f23b98b23ccbcd90ff686416dfe02
Jhonatan <jhowbrows321@gmail.com>
2025-09-21 19:21:25 -0300

    feat: implementing the pythonanywhere api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     // Para deploy, use a URL do PythonAnywhere. Para teste local, use esta:
-    baseURL: 'http://127.0.0.1:8000/api/'
+    // baseURL: 'http://127.0.0.1:8000/api/'
+    baseURL: 'https://jhowjhow.pythonanywhere.com/api/'
 });
 
 api.interceptors.request.use(async config => {
